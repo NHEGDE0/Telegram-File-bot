50604abb00b23acbe54e191a96da7149ae801865
NHEGDE0 <nhegde949@gmail.com>
2026-03-25 13:26:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
+import os
 import asyncio
-from pyrogram import Client, filters, idle
+from pyrogram import Client, filters
 
-API_ID = int("34945118")
-API_HASH = "4f40fc2316771408843047e2003d1aea"
-BOT_TOKEN = "8506591391:AAFeZ3JqlyQx_7DJzWLjF161TCVHXM-h9TQ"
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 app = Client(
-    "bot",
+    "filelinkbot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN
@@ -14,12 +15,22 @@ app = Client(
 
 @app.on_message(filters.command("start"))
 async def start(client, message):
-    await message.reply("Bot is working ✅")
+    await message.reply_text("Bot is working ✅\nSend me a file")
+
+@app.on_message(filters.document | filters.video | filters.audio)
+async def file_handler(client, message):
+    file_id = message.document.file_id if message.document else message.video.file_id if message.video else message.audio.file_id
+    
+    link = f"https://t.me/{(await client.get_me()).username}?start={file_id}"
+    
+    await message.reply_text(f"Download link:\n{link}")
 
 async def main():
     await app.start()
     print("Bot started...")
     await idle()
+
+from pyrogram import idle
 
 if name == "main":
     asyncio.run(main())
